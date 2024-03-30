efc70a65f92eca521b8591c1f16d4e821452534b
aswinr <aswinr019@gmail.com>
2024-03-30 22:29:56 +0530

    imported different roles into a single file for ease of use

--- contracts/AccessControl/Rolable.sol
+++ contracts/AccessControl/Rolable.sol
@@ -1,7 +1,27 @@
 pragma solidity ^0.8.9;
 
-import 'openzeppelin/contracts/access/AccessControl.sol'
+import './Roles/Consumer.sol';
+import './Roles/Designer.sol';
+import './Roles/Distributor.sol';
+import './Roles/Manufacturer.sol';
+import './Roles/Regulator.sol';
+import './Roles/Retailer.sol';
 
-contract Rolable is  AccessControl{
-
+contract Rolable is Consumer, Designer, Distributor, Manufacturer, Regulator, Retailer {
+  
+  function whoAmI() public view returns (
+    bool consumer, 
+    bool designer, 
+    bool distributor, 
+    bool manufacturer, 
+    bool regulator, 
+    bool retailer
+  ) {
+    consumer = amIConsumer();
+    designer = amIDesigner();
+    distributor = amiIDistributor();
+    manufacturer = amIManufacturer();
+    regulator = amIRegulator();
+    retailer = amIRetailer();
+   }
 }
