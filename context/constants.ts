6964210009c4f237d7c1d8af66351728461f8dd3
aswinr <aswinr019@gmail.com>
2024-06-15 21:22:57 +0530

    added constants file to manage address and abi imports

---
+++ context/constants.ts
@@ -0,0 +1,8 @@
+import mainChain from './MainChain.json';
+import partnership from './Partnership.json';
+
+export const mainChainAddress = '';
+export const partnershipAddress = '';
+
+export const maninChainABI = mainChain.abi;
+export const partnershipABI = partnership.abi;
