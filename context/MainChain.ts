6ef777c0342c0e528a1632400fbbd6430ca1ccdd
aswinr <aswinr019@gmail.com>
2024-06-15 22:26:12 +0530

    added templates for functions to be implemented

--- context/MainChain.ts
+++ context/MainChain.ts
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import Web3Modal from 'web3modal';
 import { ethers } from 'ethers';
+import { mainChainABI, mainChainAddress } from './constants';
+
+const fetchContract = (signerOrProvider) => {
+  new ethers.Contract( mainChainAddress, mainChainABI, signerOrProvider);
+};
 
 export const MainChainContext = React.createContext();
-export const MainChainProvider = ({ children }) => {
 
-  const [currentAccount, setCurrentAccount] = useState<string |null>('');
+export const MainChainProvider = ({ children }) => {
+  const title: string = 'Supply Chain Contract';
+  const [currentAccount, setCurrentAccount] = useState<string | null>('');
+
+  const checkIfWalletIsConnected = async () => {
+  
+    try {
+      if( !window.ethereum) return setOpenError(true), setError('Install metamask');
+    } catch(err) {}
+  };
 
   const connectWallet = async () => {
     try {} catch(err) {}
@@ -13,10 +26,66 @@ export const MainChainProvider = ({ children }) => {
   const disconnectWallet = () => {
       setCurrentAccount(null);
   };
+  const currentAccountRoles = async () => {
+    
+  };
 
-  return (
-      <div>
-      </div>
-  );
+  const addRoleToMe = async (role: string) => {
+
+  };
+
+  const removeRoleFromMe = async (role: string) => {
+
+  };
+
+  const addRegulator = async () => {};
+
+  const addDrugDesign = async () => {};
+
+  const addDrugTest = async () => {};
+
+  const addDrugTestByRegulator = async () => {};
+
+  const approveDrug = async () => {};
+
+  const sellDrugDesign = async () => {};
+
+  const buyDrugDesign = async () => {};
+
+  const updatePartnerState = async () => {};
+
+  const addPartner = async () => {};
+
+  const assignPartner = async () => {};
+
+  const manufactureDrugLoad = async () => {};
+
+  const packDrugLoad = async () => {};
+
+  const buyDrugLoad = async () => {};
+  
+  const shipDrugLoad = async () => {};
+
+  const receiveDrugLoad = async () => {};
+
+  const updateShipEnv = async () => {};
+
+  const updateStockEnv = async () => {};
+
+  const purchaseDrug = async () => {};
+
+  const fetchDrugDesignData = async () => {};
+
+  const fetchDrugLoadData = async () => {};
+
+  const getDrugLoadPKUs = async () => {};
+
+  const fetchDrugData = async () => {};
+
+  const fetchEnvHistory = async () => {};
+
+  const addTxToLogs = async () => {};
+
+  const addToLogs = async () => {};
 };
 
